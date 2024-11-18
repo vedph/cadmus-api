c341d9fa9ef7a21d77555047a169419a3bc0a0a4
Myrmex <dfusi@hotmail.com>
2024-11-18 13:16:21 +0100

    Initial commit

---
+++ Cadmus.Api.Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using Fusi.Api.Auth.Controllers;
+using Fusi.Api.Auth.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Cadmus.Api.Controllers;
+
+/// <summary>
+/// Authentication and authorization.
+/// </summary>
+/// <remarks>
+/// Initializes a new instance of the <see cref="AuthController"/> class.
+/// </remarks>
+/// <param name="userManager">The user manager.</param>
+/// <param name="roleManager">The role manager.</param>
+/// <param name="signInManager">The sign in manager.</param>
+/// <param name="configuration">The configuration.</param>
+[ApiController]
+public sealed class AuthController(UserManager<NamedUser> userManager,
+    RoleManager<IdentityRole> roleManager,
+    SignInManager<NamedUser> signInManager,
+    IConfiguration configuration) :
+    AuthControllerBase<NamedUser, IdentityRole>(
+        userManager, roleManager, signInManager, configuration)
+{
+}
---
+++ Pack.bat
@@ -0,0 +1,34 @@
+@echo off
+echo BUILD Cadmus API packages
+del .\Cadmus.Api.Config\bin\Debug\*.nupkg
+del .\Cadmus.Api.Config\bin\Debug\*.snupkg
+del .\Cadmus.Api.Controllers\bin\Debug\*.nupkg
+del .\Cadmus.Api.Controllers\bin\Debug\*.snupkg
+del .\Cadmus.Api.Controllers.Import\bin\Debug\*.nupkg
+del .\Cadmus.Api.Controllers.Import\bin\Debug\*.snupkg
+del .\Cadmus.Api.Models\bin\Debug\*.nupkg
+del .\Cadmus.Api.Models\bin\Debug\*.snupkg
+del .\Cadmus.Api.Services\bin\Debug\*.nupkg
+del .\Cadmus.Api.Services\bin\Debug\*.snupkg
+
+cd .\Cadmus.Api.Models
+dotnet pack -c Debug -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
+cd..
+
+cd .\Cadmus.Api.Services
+dotnet pack -c Debug -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
+cd..
+
+cd .\Cadmus.Api.Controllers
+dotnet pack -c Debug -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
+cd..
+
+cd .\Cadmus.Api.Controllers.Import
+dotnet pack -c Debug -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
+cd..
+
+cd .\Cadmus.Api.Config
+dotnet pack -c Debug -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
+cd..
+
+pause
