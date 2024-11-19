c7386476826fd5d03c3b9ba0ab6965be52cd2140
Myrmex <dfusi@hotmail.com>
2024-11-19 08:36:25 +0100

    Dependencies

--- PushLocal.bat
+++ PushLocal.bat
@@ -1,7 +1,7 @@
 @echo off
 echo PRESS ANY KEY TO INSTALL Cadmus Libraries TO LOCAL NUGET FEED
 echo Remember to generate the up-to-date package.
-c:\exe\nuget add .\Cadmus.Api.Config\bin\Debug\Cadmus.Api.Config.10.1.0.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Cadmus.Api.Config\bin\Debug\Cadmus.Api.Config.10.1.1.nupkg -source C:\Projects\_NuGet
 c:\exe\nuget add .\Cadmus.Api.Controllers\bin\Debug\Cadmus.Api.Controllers.10.1.0.nupkg -source C:\Projects\_NuGet
 c:\exe\nuget add .\Cadmus.Api.Controllers.Import\bin\Debug\Cadmus.Api.Controllers.Import.10.1.0.nupkg -source C:\Projects\_NuGet
 c:\exe\nuget add .\Cadmus.Api.Models\bin\Debug\Cadmus.Api.Models.10.1.0.nupkg -source C:\Projects\_NuGet
